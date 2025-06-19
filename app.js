c7ce5b1ffb16da57c84d7df2b176b4d95400a013
sumaira <sumairapathan400@gmail.com>
2025-06-19 09:08:32 +0530

    add cors library

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 import createError from 'http-errors'
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
 import authRoutes from "./routes/authRoutes.js"
+import cors from "cors"
 //ju bhi request hai usku json may convert
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use("/uploads",express.static("uploads"));
-
+app.use(cors());
 app.get("/", (req, res) => {
 })
 
